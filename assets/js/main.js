dea43982ba95ce6d12dfe441bc1e677e83ddc406
Margaret <margareth.florian@gmail.com>
2017-10-05 18:21:57 -0500

    no se

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,9 @@ $(document).ready(function(){
 			plus_width = 1500;//moviles
 		}else{
 			$('.screen-1').css('width', windowWidth);
+			$("body").css("overflow-x", "hidden");
+	    	$("body").css("overflow-y", "hidden");
+	    	$(".scroll").css("position", "fixed");	    	
 		}
 		//total_width = (windowWidth*3)+$(".gnc-biogas-landscape").width()+$(".gnc-yacimiento-landscape").width()+$(".gnc-gasoducto-landscape").width();		
 		set_width_scroll();
@@ -68,47 +71,36 @@ $(document).ready(function(){
 	
 	function set_width_scroll(){
 		console.log("SET WITH "+open_section_name+" SCROLL: open_section_width: "+windowWidth+"+"+open_section_width);
-		//if(!isMobile.any()){
+		if(isMobile.any()){
+			$(".front").css('width', windowWidth+open_section_width+plus_width); //600 for menu at the end
+		}else{
 			$(".front").css('width', windowWidth+open_section_width+plus_width); //600 for menu at the end
 		
-			$('#outer-container').css('height', eval(open_section_name+"_icons_width")+open_section_width+plus_width+'px'); //(700+open_section_width+plus_width)	 //600 for menu at the end
-		//}
+			$('#outer-container').css('height', eval(open_section_name+"_icons_width")+open_section_width+plus_width+'px'); //(700+open_section_width+plus_width)	 //600 for menu at the end			
+		}
 	}
 	//********************** end windows size
-    
+    $(".scrollTonav").hide();
     if(isMobile.any()){
-    	/*window.onscroll = function(e) {
-			  display_camion_accion();
-		}*/
-		//horizontal scroll
-	 	var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
-            {
-            //height: $(window).innerHeight(),  // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
-            onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
-                //console.log(percent);
-                
-                /*tmp = $('#track_moving').attr('data-appear-left-offset');*/
-                //tmp = Math.floor((777 - $(window).innerWidth()) * percent) * -1; 
-             	display_camion_accion();
-            } //end onScroll
-            
-        }); //end scroll invert	
+    	window.onscroll = function(e) {
+    		console.log("onscroll: "+window.pageXOffset);
+			display_camion_accion(window.pageXOffset);
+		}	
     }else{
-    	$(".scrollTonav").hide();
     	//horizontal scroll
 	 	var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
             {            
             onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
-             	display_camion_accion();
+             	var position = $(".front").position();    	
+                console.log("position lef: "+(position.left*-1));
+                integer_position_left = position.left*-1;                        
+             	display_camion_accion(integer_position_left);
             } //end onScroll
             
         }); //end scroll invert	
     }
     
-    function display_camion_accion(){
-    	var position = $(".front").position();
-                console.log("position lef: "+(position.left*-1));
-                integer_position_left = position.left*-1; 
+    function display_camion_accion(integer_position_left){
                 if((open_section_name=="biogas" && integer_position_left >= 5100) || (open_section_name=="yacimiento" && integer_position_left > 6000) || (open_section_name=="gasoducto" && integer_position_left > 6034) ){//|| integer_position_left > 17400     //biogas 5100           	
 					if( (open_section_name=="biogas" && integer_position_left > 13990) || (open_section_name=="yacimiento" && integer_position_left > 14855) ||(open_section_name=="gasoducto" && integer_position_left > 15565) ){ // && integer_position_left < 13700
 							console.log(open_section_name+": camion para");						
