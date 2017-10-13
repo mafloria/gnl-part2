d66f662f5a729e0430d2613189d682c0ba523bca
Margaret <margareth.florian@gmail.com>
2017-10-13 09:32:11 -0500

    reducir yacimientos en moviles

--- assets/js/main-gnl.js
+++ assets/js/main-gnl.js
@@ -57,6 +57,8 @@ $(document).ready(function(){
 			$(".hand-instruction").show();
 			plus_width = -300;//moviles
 			
+			if(open_section_name=="yacimiento") yacimiento_icons_width = 1000;
+			
 			//let all animations fixed
 			$(".fadeInLeftShort").removeClass("animated");
 	    	$(".fadeInLeftShort").removeClass("fadeInLeftShort");
